fd46a52ad30c21f030d92fa522d7fcdb15886570
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-07-20 22:44:48 +0200

    add basic colors

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -2,6 +2,45 @@ export default {
     theme: "auto",
 
     colors: {
+        "primary": {
+            dark: "#2323FF",
+            light: "#305CDE",
+        },
+
+        "primary-dark": {
+            dark: "#3E51C9",
+            light: "#274AB3",
+        },
+
+        "primary-light": {
+            dark: "#99A7FF",
+            light: "#59B5F7",
+        },
+
+        "background": {
+            dark: "#121212",
+            light: "#FFFFFF",
+        },
+
+        "background-one": {
+            dark: "#1A1A1A",
+            light: "#F2F2F2",
+        },
+
+        "background-two": {
+            dark: "#1F1F1F",
+            light: "#E6E6E6",
+        },
+
+        "text": {
+            dark: "#E0E0E0",
+            light: "#000000",
+        },
+
+        "text-one": {
+            dark: "#E0E0E0",
+            light: "#000000",
+        },
     },
 
     getSystemTheme() {
@@ -18,7 +57,7 @@ export default {
 
     setColors() {
         for (const [key, value] of Object.entries(this.colors)) {
-            document.documentElement.style.setProperty(`--${key}`, value);
+            document.documentElement.style.setProperty(`--${key}`, value[this.getTheme()]);
         }
     }
 }
