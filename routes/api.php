a7653df8c439a92301bd65fb043886a06a848734
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-07-25 23:18:13 +0200

    expire date

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/user/token/{token}', function (Request $request, $token) {
 
@@ -13,13 +14,17 @@ Route::get('/user/token/{token}', function (Request $request, $token) {
         return response()->json(['message' => 'Token not found'], 404);
     }
 
+    // Get the expeation date time
+    $expiresAt = $token->expires_at;
+    if (now()->greaterThan($expiresAt)) {
+        return response()->json(['message' => 'Token has expired'], 401);
+    }
+
     $userID = $token->user_id;
-
-
     $user = \App\Models\User::find($userID);
-
 
     return response()->json([
         'user' => $user,
-    ]);
+    ], 200)
+    ->header('Content-Type', 'application/json');
 });
