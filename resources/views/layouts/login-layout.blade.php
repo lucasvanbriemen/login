43747a12198d7b0c4bbfdf0267088245f15e8ce7
Lucas van Briemen <contact@lucasvanbriemen.nl>
2025-07-26 12:21:27 +0200

    add favicon

--- resources/views/layouts/login-layout.blade.php
+++ resources/views/layouts/login-layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }}</title>
+
+    <link rel="icon" href="{{ asset('images/login.svg') }}">
 </head>
 
 <body class="{{ $bodyClass }}">
