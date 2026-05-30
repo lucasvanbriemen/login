f835772f8caa49e3c638a1ad8319716d03fc9eca
Lucas <contact@lucasvanbriemen.nl>
2026-05-30 11:43:04 +0200

    Allow for logging in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    account = Account.find_by(email: params[:email])
+
+    if account&.authenticate(params[:password])
+      session[:account_id] = account.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash.now[:alert] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
