eb310d677938c511509186e704b7052b2e95f824
Lucas <contact@lucasvanbriemen.nl>
2026-05-30 13:59:13 +0200

    Allow other hosts.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,7 @@ class SessionsController < ApplicationController
 
     path = params[:redirect_to] || root_path
 
-    redirect_to path.to_s + "?auth_token=#{token.value}", notice: "Logged in successfully"
+    redirect_to path.to_s + "?auth_token=#{token.value}", notice: "Logged in successfully", allow_other_host: true
   end
 
   def show
